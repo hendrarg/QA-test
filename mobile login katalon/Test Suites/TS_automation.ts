df6d1617b883a9aef489b2749691154f52b6d97b
hendrarg <jendraljohn92@gmail.com>
2024-01-31 11:41:56 +0700

    first commit

---
+++ mobile login katalon/Test Suites/TS_automation.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_automation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7e817b4c-6ad9-4278-91f7-0510d7c4c5f0</testSuiteGuid>
+   <testCaseLink>
+      <guid>6e6a1eb8-9d01-4996-b10e-b2f39dde8d02</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Positive Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5ce00a19-4218-4fbf-817a-7558f7e03f39</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Negative Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ readme.md
@@ -0,0 +1,8 @@
+## automation test API
+
+I prefer API automation testing using Postman because it is easier to do manual checks and regression testing with one tool. The features provided are more complete, such as reports and can be integrated with other tools.
+
+## automation mobile
+
+iam using katalon studion and appium. folder mobile login includ test and report positive negative
+
